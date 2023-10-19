6d9ab236ae5315729f9a9d4c75a0627b51f0769a
Virgo41 <43315050+Virgo41@users.noreply.github.com>
2023-10-19 17:29:31 +0800

    Update patch_perseus.sh

--- patch_perseus.sh
+++ patch_perseus.sh
@@ -24,18 +24,10 @@ done
 
 chmod +x apkeep
 
-# Download Azur Lane
-download_azurlane () {
-    if [ ! -f "com.YoStarJP.AzurLane" ]; then
-    ./apkeep -a com.YoStarJP.AzurLane .
-    fi
-}
-
 if [ ! -f "com.YoStarJP.AzurLane" ]; then
     echo "Get Azur Lane apk"
-    download_azurlane
-    unzip -o com.YoStarJP.AzurLane.xapk -d AzurLane
-    cp AzurLane/com.YoStarJP.AzurLane.apk .
+    wget https://dl-pc-sz-cf.pds.quark.cn/4qd6MFll/5632021470/6530f4ddfa46bf254bce4d3a9751765d7ce0bda4/6530f4dd3db63b21eaca4d3b88db9fa5ce7c4070?Expires=1697728901&OSSAccessKeyId=LTAIyYfxTqY7YZsg&Signature=zMHHLZG7gTzLJb37H4B94oLXbVs%3D&x-oss-traffic-limit=503316480&response-content-disposition=attachment%3B%20filename%3Dbase.apk&callback-var=eyJ4OmF1IjoiLSIsIng6dWQiOiIxNi0wLTYtMC04LU4tNC1OLTEtMTYtMC1OIiwieDpzcCI6IjE5OSIsIng6dG9rZW4iOiI0LTRjYmQ0ZGRkYTlkMGU2ZTczNzViMTY4MzAxZWU5MzA5LTgtMS01MDAtNzYyMTVlNzIwNTdkNDM3ZTg1NmY2ZWM1Y2Q0MTFkYTYtMC0wLTAtMC1hNDY0MDY2MmU2YzRmMjA1ODI5MDI5Nzc3ZWMzZDRlZiIsIng6dHRsIjoiMjE2MDAifQ%3D%3D&callback=eyJjYWxsYmFja0JvZHlUeXBlIjoiYXBwbGljYXRpb24vanNvbiIsImNhbGxiYWNrU3RhZ2UiOiJiZWZvcmUtZXhlY3V0ZSIsImNhbGxiYWNrRmFpbHVyZUFjdGlvbiI6Imlnbm9yZSIsImNhbGxiYWNrVXJsIjoiaHR0cHM6Ly9jbG91ZC1hdXRoLmRyaXZlLnF1YXJrLmNuL291dGVyL29zcy9jaGVja3BsYXkiLCJjYWxsYmFja0JvZHkiOiJ7XCJob3N0XCI6JHtodHRwSGVhZGVyLmhvc3R9LFwic2l6ZVwiOiR7c2l6ZX0sXCJyYW5nZVwiOiR7aHR0cEhlYWRlci5yYW5nZX0sXCJyZWZlcmVyXCI6JHtodHRwSGVhZGVyLnJlZmVyZXJ9LFwiY29va2llXCI6JHtodHRwSGVhZGVyLmNvb2tpZX0sXCJtZXRob2RcIjoke2h0dHBIZWFkZXIubWV0aG9kfSxcImlwXCI6JHtjbGllbnRJcH0sXCJwb3J0XCI6JHtjbGllbnRQb3J0fSxcIm9iamVjdFwiOiR7b2JqZWN0fSxcInNwXCI6JHt4OnNwfSxcInVkXCI6JHt4OnVkfSxcInRva2VuXCI6JHt4OnRva2VufSxcImF1XCI6JHt4OmF1fSxcInR0bFwiOiR7eDp0dGx9LFwiZHRfc3BcIjoke3g6ZHRfc3B9LFwiY2xpZW50X3Rva2VuXCI6JHtxdWVyeVN0cmluZy5jbGllbnRfdG9rZW59fSJ9&ud=16-0-6-0-8-N-4-N-1-16-0-N -O com.YoStarJP.AzurLane.apk -q
+    echo "apk downloaded !"
 fi
 
 # Download Perseus
